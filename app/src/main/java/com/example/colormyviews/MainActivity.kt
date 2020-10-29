f60604cb5e8abe0d01be533eabfa9de491040bc6
Juliana Cuani <juliana.271@hotmail.com>
2020-10-29 20:09:01 -0300

    adicionando botão para colorir

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.colormyviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var currentColor = R.color.grey
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setColorButtonsAction()
+    }
+
+    private fun setColorButtonsAction() {
+        red_button.setOnClickListener {
+            currentColor = R.color.red
+        }
+
+        yellow_button.setOnClickListener {
+            currentColor = R.color.yellow
+        }
+
+        green_button.setOnClickListener {
+            currentColor = R.color.green
+        }
     }
 }
